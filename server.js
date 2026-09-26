36cedd2b4b6c9db043074277136f580c12a76719
Agbidye Tor Emmanuel <64062941+agbidyetor@users.noreply.github.com>
2026-09-26 16:33:34 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -467,3 +467,8 @@ const server = http.createServer((request, response) => {
 });
 
 server.listen(port, () => console.log(`Shanghai Investment running at http://localhost:${port}`));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
